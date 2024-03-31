813d29834fc4585c22bf3a07d63dcd1855a68b0a
bitttttten <bitttttten@googlemail.com>
2024-03-31 23:39:20 +0200

    wip

---
+++ index.mjs
@@ -0,0 +1,12 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(3000);
+
+console.log("Server running on port 3000");
